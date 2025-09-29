6cda3ab17c92278f04fa7a0dbb5f3e61870deb7e
ibrahimallemi <iroallemi@gmail.com>
2025-09-29 11:19:16 +0100

    Add files via upload

---
+++ day9/day9.js
@@ -0,0 +1,46 @@
+// Day 9 – Variables, Data Types, Operators Practice
+
+// Practical 1
+let username = "Ibrahim";
+console.log("Practical 1 → Username:", username);
+
+// Practical 2
+let birthYear = 2010;
+let age = 2025 - birthYear;
+console.log("Practical 2 → Age is:", age);
+
+// Practical 3
+let greeting = "Hello" + " " + "World";
+console.log("Practical 3 → Greeting:", greeting);
+
+// Practical 4
+let length = 12;
+let width = 5;
+let area = length * width;
+console.log("Practical 4 → Area:", area);
+
+// Practical 5
+console.log("Practical 5 →", 10 > 5 && 10 < 20);
+
+// Practical 6
+let marks = 50;
+marks += 20;
+console.log("Practical 6 → Marks:", marks);
+
+// Practical 7
+console.log("Practical 7 → 15 == '15':", 15 == "15");
+console.log("Practical 7 → 15 === '15':", 15 === "15");
+
+// Practical 8
+let isLoggedIn = false;
+console.log("Practical 8 → !isLoggedIn:", !isLoggedIn);
+
+// Practical 9
+let grade = 90;
+let result = "Your grade is " + grade;
+console.log("Practical 9 →", result);
+
+// Practical 10
+let personAge = 18;
+let isAdult = personAge >= 18;
+console.log("Practical 10 → Adult:", isAdult);
